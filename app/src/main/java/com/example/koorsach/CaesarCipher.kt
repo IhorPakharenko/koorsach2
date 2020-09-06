d23a0f678b7273405700b6fa513ace1002326a67
Ihor Pakharenko <igor.pakharenko@ekreative.com>
2020-09-06 23:38:55 +0300

    Break CaesarCipher.kt's beauty and conciseness by matching task requirements...

--- app/src/main/java/com/example/koorsach/CaesarCipher.kt
+++ app/src/main/java/com/example/koorsach/CaesarCipher.kt
@@ -4,39 +4,62 @@ import java.math.BigInteger
 
 object CaesarCipher {
     fun encrypt(toEncrypt: String, alphabet: String, key: BigInteger): Result {
-        val offset = getOffset(key, alphabet)
-        return Result(text = shift(toEncrypt, alphabet, offset), offset = offset)
+        val operation = Encryption(toEncrypt, alphabet, key)
+        return Result(text = operation.applyOperation(), offset = operation.offset)
     }
 
     fun decrypt(toDecrypt: String, alphabet: String, key: BigInteger): Result {
-        val offset = getOffset(key.unaryMinus(), alphabet)
-        return Result(text = shift(toDecrypt, alphabet, offset), offset = offset)
+        val operation = Decryption(toDecrypt, alphabet, key)
+        return Result(text = operation.applyOperation(), offset = operation.offset)
     }
 
-    private fun shift(original: String, alphabet: String, offset: Int) =
-        original.map { originalChar ->
-            val charIndexInAlphabet = alphabet.indexOf(originalChar, ignoreCase = true)
-            if (charIndexInAlphabet >= 0) {
-                val unsafeIndex = charIndexInAlphabet + offset
-                val index = when {
-                    unsafeIndex > alphabet.lastIndex -> unsafeIndex - alphabet.length
-                    unsafeIndex < 0 -> unsafeIndex + alphabet.length
-                    else -> unsafeIndex
-                }
-                alphabet[index].let { newChar ->
-                    if (originalChar.isUpperCase()) {
-                        newChar.toUpperCase()
-                    } else {
-                        newChar
-                    }
-                }
-            } else {
-                originalChar
-            }
-        }.joinToString("")
+    abstract class CipherOperation(
+        private val original: String,
+        private val alphabet: String,
+        private val key: BigInteger
+    ) {
+        abstract val offset: Int
 
-    private fun getOffset(key: BigInteger, alphabet: String) =
-        (key % alphabet.length.toBigInteger()).toInt()
+        fun applyOperation() = shift(original, alphabet, offset)
+
+        fun shift(original: String, alphabet: String, offset: Int) =
+            original.map { originalChar ->
+                val charIndexInAlphabet = alphabet.indexOf(originalChar, ignoreCase = true)
+                if (charIndexInAlphabet >= 0) {
+                    val unsafeIndex = charIndexInAlphabet + offset
+                    val index = when {
+                        unsafeIndex > alphabet.lastIndex -> unsafeIndex - alphabet.length
+                        unsafeIndex < 0 -> unsafeIndex + alphabet.length
+                        else -> unsafeIndex
+                    }
+                    alphabet[index].let { newChar ->
+                        if (originalChar.isUpperCase()) {
+                            newChar.toUpperCase()
+                        } else {
+                            newChar
+                        }
+                    }
+                } else {
+                    originalChar
+                }
+            }.joinToString("")
+    }
+
+    class Encryption(
+        original: String,
+        alphabet: String,
+        key: BigInteger
+    ) : CipherOperation(original, alphabet, key) {
+        override val offset = (key % alphabet.length.toBigInteger()).toInt()
+    }
+
+    class Decryption(
+        original: String,
+        alphabet: String,
+        key: BigInteger
+    ) : CipherOperation(original, alphabet, key) {
+        override val offset = (key % alphabet.length.toBigInteger()).toInt().unaryMinus()
+    }
 
     data class Result(val text: String, val offset: Int)
 }
